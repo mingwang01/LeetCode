94273d0586165864ed2fd1b07554263d88cc44c5
mingwang01 <zxmgzd@163.com>
2022-05-05 22:34:26 +0800

    俩数之和

--- src/main/java/leetcode/addTwoNumbers/AddTwoNumbers.java
+++ src/main/java/leetcode/addTwoNumbers/AddTwoNumbers.java
@@ -25,54 +25,57 @@ public class AddTwoNumbers {
         l2b.val = 4;
 
         AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
-        addTwoNumbers.addTwoNumbers(l1,l2);
+        addTwoNumbers.addTwoNumbers(l1, l2);
     }
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-         int a = 0;
-        while (true){
-            if (l1 != null && l2 != null){
-                l1.val = (l1.val+ l2.val+a)%10;
-                a = ((l1.val+ l2.val+a) -(l1.val+ l2.val+a)%10)/10;
-                l1 = l1.next;
-                l2 = l2.next;
-            }else {
-                if (l1 == null && l2 != null){
-                    if (l1.val + a>= 10){
-                        a = (l1.val + a)% 10;
-                        l1.val = ((l1.val+ a) -(l1.val+a)%10)/10;
-                        l1 = l1.next;
-                    }else {
-                        l1 = l2;
-                        l1.val = l1.val+a;
-                        break;
-                    }
-
-                } else if (l1 != null && l2 == null){
-                    if (l1.val + a>=10){
-                        
-                    }
-                    break;
-                }else {
-                    if (a>0){
-                        ListNode listNode = new ListNode();
-                        l1 = listNode;
-                        l1.val = a;
-                    }
-                    break;
-
-                }
-            }
-
+        Long a = 0L;
+        int flag = 1;
+       while (true){
+           if (l1 != null || l2 != null){
+               if (l1 != null ){
+                   a = a + l1.val*flag;
+                   if (l1.next != null){
+                       l1 = l1.next;
+                   }
+               }
+               if (l2 != null){
+                   a = a + l1.val*flag;
+                   if (l1.next != null){
+                       l1 = l1.next;
+                   }
+               }
+               flag = flag * 10;
+           }
+           if (l1 != null && l2 !=null){
+               break;
+           }
+       }
+        String str = a.toString();
+        ListNode listNode = new ListNode();
+        for (int s = 0; s < str.length(); s++) {
+            //这里有问题
+            listNode.val = Integer.valueOf(str.substring(s,s+1));
+            ListNode listNode1 = new ListNode();
+            listNode.next = listNode1;
         }
-        return l1;
+        return listNode;
     }
 }
+    class ListNode {
+        int val;
+        ListNode next;
 
-   class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-  }
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
--- src/main/java/sync/casdemo/AccountTest.java
+++ src/main/java/sync/casdemo/AccountTest.java
@@ -13,16 +13,12 @@ public class AccountTest {
     }
     static void test (Account account){
         List<Thread> ts = new ArrayList<Thread>();
-        for (int i = 0; i < 500; i++) {
-            ts.add(new Thread(() -> {
-                account.acquire(20);
-            }));
-        }
+
         for (Thread t:ts){
             t.start();
         }
         for (Thread t:ts){
-            t.join();
+
         }
         System.out.println(account.query());
     }
